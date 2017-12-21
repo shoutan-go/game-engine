9138af1620a7dc8389add09cfc06612cbb41f347
Lookis <lookisliu@gmail.com>
2017-12-22 00:28:11 +0800

    build webpack with umd

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["GameEngine"] = factory();
+	else
+		root["GameEngine"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -398,3 +408,4 @@ module.exports = exports['default'];
 
 /***/ })
 /******/ ]);
+});
